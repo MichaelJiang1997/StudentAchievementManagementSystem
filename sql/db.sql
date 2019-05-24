b1a97bf1af237cd7179a862cbe361d0e47203cc9
Michael Jiang <39025275+MichaelJiang1997@users.noreply.github.com>
2019-05-25 07:57:07 +0800

    Update db.sql

--- sql/db.sql
+++ sql/db.sql
@@ -1,39 +1,33 @@
-//Êý¾Ý¿â
+//æ•°æ®åº“
 create database xscjglxt default charset utf8;
 
-//Ñ§Éú±í
+//å­¦ç”Ÿè¡¨
 create table stu_info(
 	sid varchar(8) primary key,
 	sname varchar(10) not null,
 	sgender varchar(2) not null
 	);
 
-//³É¼¨±í
+//æˆç»©è¡¨
 create table stu_score(
 	sid varchar(8) not null,
 	cid varchar(8) not null,
 	grade int
 	);
 
-//¿Î³Ì±í
+//è¯¾ç¨‹è¡¨
 create table stu_course(
 	cid varchar(8) primary key,
 	cname varchar(20) not null
 	);
 
-//¹ÜÀíÔ±±í
+//ç®¡ç†å‘˜è¡¨
 create table admin(
 	aid varchar(8) primary key,
 	apsw varchar(20) not null
 	);
 	
 	
-	
-select stu_info.sid,sname,sgender,stu_score.cid,cname,grade 
-from stu_info,stu_score,stu_course 
-where stu_score.cid="201" and stu_info.sid = stu_score.sid and stu_score.cid = stu_course.cid
-and grade > 30;
-
 
 
 
